edd56cfb62687601fe90f77cba1c45d06c294e11
Chad Cramer <chcr6493@colorado.edu>
2019-05-06 12:23:57 -0500

    tictactoe start

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,51 @@
+let button = document.getElementById('button');
+let squareOne = document.getElementById('one');
+let squareTwo = document.getElementById('two');
+let squareThree = document.getElementById('three');
+let squareFour = document.getElementById('four');
+let squareFive = document.getElementById('five');
+let squareSix = document.getElementById('six');
+let squareSeven = document.getElementById('seven');
+let squareEight = document.getElementById('eight');
+let squareNine = document.getElementById('nine');
 
+button.addEventListener("click", () => {
+    console.log('HELLO');
+})
+
+squareOne.addEventListener("click", () => {
+    console.log('ONE');
+})
+
+squareTwo.addEventListener("click", () => {
+    console.log('TWO');
+})
+
+squareThree.addEventListener("click", () => {
+    console.log('THREE');
+})
+
+squareFour.addEventListener("click", () => {
+    console.log('FOUR');
+})
+
+squareFive.addEventListener("click", () => {
+    console.log('FIVE');
+})
+
+squareSix.addEventListener("click", () => {
+    console.log('SIX');
+})
+
+squareSeven.addEventListener("click", () => {
+    console.log('Seven');
+})
+
+squareEight.addEventListener("click", () => {
+    console.log('EIGHT');
+})
+
+squareNine.addEventListener("click", () => {
+    console.log('NINE');
+})
 
