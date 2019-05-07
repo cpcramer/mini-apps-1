75ccfc6bc924286308cfbfe3d4f416cd8dde4069
Chad Cramer <chcr6493@colorado.edu>
2019-05-07 18:51:42 -0500

    challenge 2, full day progress

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,16 +1,30 @@
 let submitButton = document.getElementById('submitButton');
 
+// submitButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     let data = document.getElementById("textbox").value;
+//     fetch('http://localhost:3001/', {
+//         method: "POST", 
+//         mode: "no-cors", 
+//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//         body: data, 
+//     }).then( (response) => {
+//         console.log(response.json())
+//         console.log(response.text())
+//     })
+// })
+
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     let data = document.getElementById("textbox").value;
-    fetch('http://localhost:3001/', {
+    $.ajax({
         method: "POST", 
+        url: 'http://localhost:3001/',
         mode: "no-cors", 
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: data, 
-    }).then( (response) => {
-        console.log(response.json())
-        console.log(response.text())
-    })
-})
-
+        body: JSON.stringify(data),
+        success: (res) => {
+            console.log(res);            
+        }
+    });
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 // you need to use bodyParser() if you want the form data to be available in req.body
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,9 +19,10 @@ app.use(function(req, res, next) {
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/', (req, res) => {
-
     console.log(req.body);
-    res.send('testing testing');
+    // res.myData = "";
+    // res.myData = req.body;
+    res.send(req.body);
 
 });
 
