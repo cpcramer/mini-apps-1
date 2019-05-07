90822a7365875c426aec4aa94b808f6497fb8913
Chad Cramer <chcr6493@colorado.edu>
2019-05-07 12:26:18 -0500

    challenge2 progress

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,14 @@
+// may use jQuery
 
+let submitButton = document.getElementById('submitButton');
 
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let data = document.getElementById("textbox").value;
+    fetch('http://localhost:3001/', {
+        method: "POST", 
+        body: JSON.stringify(data), 
+    }).then( (response) => {
+        console.log(response);
+    });
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const path = require('path');
+const bodyParser = require('body-parser');
 
+app.use(express.static(path.join(__dirname, 'client')));
 
+// you need to use bodyParser() if you want the form data to be available in req.body
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/', (req, res) => {
+    //To specify what origins have access to the resource, you need to add the Access-Control-Allow-Origin header to your response. It will be interpreted by the browser of the visitor of your site.
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    console.log(req.body);
+    res.send();
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
